e23a9c19c39da084eb9b8086ee2a9fe42d3156f5
danilobrinu <ddbn.c2@gmail.com>
2019-08-18 15:42:14 -0500

    Update main component

--- App.js
+++ App.js
@@ -1,41 +1,65 @@
-import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { Platform, StyleSheet, Text, Button, View } from 'react-native';
+import * as Print from './expo-agnostic/expo-print';
+import * as Facebook from './expo-agnostic/expo-facebook';
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import './third-party/facebook-sdk';
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu",
-  web: "Expo Web"
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+  web: (
+    <Text accessibilityRole="button" style={{ color: 'blue' }} onPress={() => location.reload()}>
+      Click in this text to reload
+    </Text>
+  ),
 });
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text style={styles.instructions}>{instructions}</Text>
+      <Button
+        color="#1877f2"
+        title="Facebook login"
+        onPress={async () => {
+          console.log(await Facebook.logInWithReadPermissionsAsync('2084560051617430'));
+        }}
+      />
+      <Button
+        color="#8e44ad"
+        title="Print"
+        onPress={async () => {
+          console.log(await Print.printAsync());
+        }}
+      />
+    </View>
+  );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
-    margin: 10
+    textAlign: 'center',
+    margin: 10,
   },
   instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
