fe9bf1c9e10036bd95de3a970718c5a30882f977
danilobrinu <ddbn.c2@gmail.com>
2019-08-27 10:42:32 -0500

    Minor changes

--- App.js
+++ App.js
@@ -13,7 +13,11 @@ const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
   web: (
-    <Text accessibilityRole="button" style={{ color: 'blue' }} onPress={() => location.reload()}>
+    <Text
+      accessibilityRole="button"
+      style={{ color: 'blue' }}
+      onPress={() => window.location.reload()}
+    >
       Click in this text to reload
     </Text>
   ),
